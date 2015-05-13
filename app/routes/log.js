40b03b772acebc0c077198eaa07a5e2803f1f5da
go1dfish <justageekbiz@gmail.com>
2015-05-13 14:59:51 -0700

    Use HTTPs

--- app/routes/log.js
+++ app/routes/log.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
   model: function(args) {
     var url = 'http://www.reddit.com/r/' + args.subreddit + '/about/log/.json?feed=' + config.redditFeed +'&user=' + config.redditUser + '&limit=100';
     return Ember.RSVP.resolve(Ember.$.ajax({
-      url: 'http://jsonp.afeld.me/?url=' + encodeURIComponent(url),
+      url: 'https://jsonp.afeld.me/?url=' + encodeURIComponent(url),
       dataType: 'json'
     })).then(function(result) {
       return result.data.children.getEach('data');
