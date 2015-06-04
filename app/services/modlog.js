f04974c2a8122622b7183fbfdef6e988290bce48
go1dfish <justageekbiz@gmail.com>
2015-06-03 23:01:33 -0700

    elegance

--- app/services/modlog.js
+++ app/services/modlog.js
@@ -10,15 +10,49 @@ export default Ember.Service.extend({
     });
   }.observes('snoocore.api').on('init'),
 
+  multis: {},
+
+  subs: function() {
+    return ['snew', 'modlog'].concat(this.getMulti('self')).concat(this.getMulti('link')).uniq().sort();
+  }.property('multis', 'multis'),
+
+  getMulti: function(name) {
+    return this.get('multis.' + name.toLowerCase()) || [];
+  },
+
+  fetchMultis: function() {
+    var anon = this.get('snoocore.anon');
+    var self = this;
+    return anon('/api/multi/user/PoliticBot').get({
+    }).then(function(result) {
+      return result.getEach('data').map(function(multi) {
+        multi.subreddits = (multi.subreddits || []).getEach('name').map(function(j) {
+          return j.toLowerCase();
+        });
+        return multi;
+      });
+    }).then(function(multis) {
+      var result = {};
+      multis.forEach(function(multi) {
+        result[multi.name.toLowerCase()] = multi.subreddits;
+      });
+      return result;
+    }).then(function(multis) {
+      self.set('multis', multis);
+      return multis;
+    });
+  }.on('init'),
+
   scanUrl: function(url) {
     var anon = this.get('snoocore.anon');
     var snoo = this.get('snoocore.api');
+    var self = this;
     return anon('/api/info').get({url: url}).then(function(result) {
-      return result.data.children.getEach('data');
+      return (result.data.children || []).getEach('data');
     }).then(function(known) {
       return known.filter(function(item) {return item.author !== '[deleted]';});
     }).then(function(known) {
-      if (!known.length) {throw 'No known posts for ' + url;}
+      return;
       if (known.length === 1) {
         var item = known[0];
         if (!item.over_18 && !item.domain.match(/(imgur|reddit.com)/)) {
@@ -31,51 +65,20 @@ export default Ember.Service.extend({
             sendreplies: false
           });
         }
-        throw 'Not enough posts for ' + url;
+        return;
       }
       var mirror = known.get('firstObject.id');
       return anon('/duplicates/$article').listing({
         $article: mirror, limit: 100
       }, {listingIndex: 1}).then(function(dupes) {
-        return dupes.children.getEach('data');
+        return (dupes.children || []).getEach('data');
       }).then(function(dupes) {
+        if (!known) {return [];}
         var knownIds = known.getEach('id');
         var dupeIds = dupes.getEach('id').concat([mirror]);
         var removedIds = knownIds.slice().removeObjects(dupeIds);
-        var politic = known.findProperty('subreddit', 'politics');
-        var worldnews = known.findProperty('subreddit', 'worldnews');
-        var til = known.findProperty('subreddit', 'todayilearned');
-
-        if (politic) {
-          snoo('/api/submit').post({
-            sr: 'politic',
-            kind: 'link',
-            title: (politic.title).slice(0, 299),
-            url: politic.url,
-            extension: 'json',
-            sendreplies: false
-          });
-        }
-        if (worldnews) {
-          snoo('/api/submit').post({
-            sr: 'FORTWorldNews',
-            kind: 'link',
-            title: (worldnews.title).slice(0, 299),
-            url: worldnews.url,
-            extension: 'json',
-            sendreplies: false
-          });
-        }
-        if (til) {
-          snoo('/api/submit').post({
-            sr: 'FORT_til',
-            kind: 'link',
-            title: (til.title).slice(0, 299),
-            url: til.url,
-            extension: 'json',
-            sendreplies: false
-          });
-        }
+        var known = known.sortBy('sort:desc');
+        self.processPosts(known);
         return removedIds.map(function(id) {
           return known.findProperty('id', id);
         });
@@ -101,12 +104,14 @@ export default Ember.Service.extend({
     var reported = this.get('reported');
     var snoo = this.get('snoocore.api');
     function loop() {
-      var shouldReport = modlog.get('snoocore.isLoggedIn')
-      return modlog.scanListing(listing, detected, before).then(function(removed) {
+      var shouldReport = modlog.get('snoocore.isLoggedIn');
+      return modlog.fetchMultis().then(function() {
+        return modlog.scanListing(listing, detected, before);
+      }).then(function(removed) {
         before = removed.nextbefore;
-        return removed.filter(function(item) {
+        return (removed || []).filter(function(item) {
           return !detected.findProperty('id', item.id);
-        });
+        })
       }).then(loop);
     }
     return loop();
@@ -202,57 +207,73 @@ export default Ember.Service.extend({
     });
   },
 
+  processPosts: function(posts) {
+    var snoo = this.get('snoocore.api');
+    var self = this;
+    var linksubs = this.getMulti('link');
+    var selfsubs = this.getMulti('self');
+    console.log(linksubs, selfsubs, posts.getEach('subreddit').uniq());
+    this.getMulti('link').forEach(function(sub) {
+      return posts.filterProperty('is_self', false).filter(function(item) {
+        return !!item.url && self.getMulti(sub).contains(item.subreddit.toLowerCase());
+      }).forEach(function(item) {
+        console.log('link', item);
+        return snoo('/api/submit').post({
+          sr: sub,
+          kind: 'link',
+          title: (item.title).slice(0, 299),
+          url: item.url,
+          extension: 'json',
+          sendreplies: false
+        });
+      });
+    });
+    this.getMulti('self').forEach(function(sub) {
+      posts.filterProperty('is_self', true).filter(function(item) {
+        return self.getMulti(sub).contains(item.subreddit.toLowerCase());
+      }).forEach(function(item) {
+        console.log('self', item);
+        return snoo('/api/submit').post({
+          sr: sub,
+          kind: 'link',
+          title: item.title,
+          url: 'https://us.reddit.com' + item.permalink + '#' + item.subreddit + '|' + item.author,
+          extension: 'json',
+          sendreplies: false
+        });
+      });
+    });
+  },
+
   scanListing: function(listing, detected, before) {
     var anon = this.get('snoocore.anon');
     var snoo = this.get('snoocore.api');
     var modlog = this;
+    var self = this;
     detected = detected || [];
     return anon('/r/' + listing).listing({
-      limit: 10,
+      limit: 100,
       before: before
     }).then(function(slice) {
-      return slice.children.getEach('data');
+      return (slice.children || []).getEach('data');
     }).then(function(posts) {
       return posts.filterProperty('is_self', false);
     }).then(function(posts) {
       return posts;//.filterProperty('over_18', false);
     }).then(function(posts) {
       before = posts.get('firstObject.name');
+      self.processPosts(posts);
       return posts.getEach('author').uniq().without('[deleted]');
     }).then(function(authors) {
       return Ember.RSVP.all(authors.map(function(author) {
         return anon('/user/' + author + '/overview').listing({
           limit: 100
         }).then(function(slice) {
-          return slice.children.getEach('data');
+          return (slice.children || []).getEach('data');
         }).then(function(items) {
-          var urls = items.getEach('url').without(undefined).uniq().slice(0, 10);
+          var urls = items.filterProperty('is_self', false).getEach('url').without(undefined).uniq().slice(0, 10);
           var comments = items.filter(function(item) {
             return !!item.parent_id;
-          });
-          items.filter(function(item) {
-            return (item.is_self && item.subreddit === 'AskReddit');
-          }).forEach(function(item) {
-            snoo('/api/submit').post({
-              sr: 'FORTAskReddit',
-              kind: 'link',
-              title: (item.title).slice(0, 299),
-              url: 'https://us.reddit.com' + item.permalink + '#AskReddit|' + item.author,
-              extension: 'json',
-              sendreplies: false
-            });
-          });
-          items.filter(function(item) {
-            return (item.is_self && item.subreddit === 'TheoryOfReddit');
-          }).forEach(function(item) {
-            snoo('/api/submit').post({
-              sr: 'FORTtheory',
-              kind: 'link',
-              title: (item.title).slice(0, 299),
-              url: 'https://us.reddit.com' + item.permalink + '#TOR|' + item.author,
-              extension: 'json',
-              sendreplies: false
-            });
           });
           var after = null;
           comments = comments.map(function(comment) {
@@ -277,14 +298,16 @@ export default Ember.Service.extend({
             });
           })).then(function(removedLists) {
             var removed = [];
+            if (!removedLists) {return;}
             removedLists.forEach(function(items) {
-              removed.addObjects(items.filter(function(post) {
+              removed.addObjects((items || []).filter(function(post) {
                 return !detected.findProperty('id', post.id);
               }));
             });
             detected.addObjects(removed);
             return removed;
           }).then(function(removed) {
+            if (!removed) {return [];}
             if (!comments.length) {return removed;}
             return modlog.findMissingComments(comments).then(function() {
               return removed;
