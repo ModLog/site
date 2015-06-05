88f96dd71c5a7dc0432c5ef0a4b5ad14de28d150
go1dfish <justageekbiz@gmail.com>
2015-06-04 18:27:49 -0700

    Add news filter

--- app/components/profs-gradio.js
+++ app/components/profs-gradio.js
@@ -3,10 +3,26 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   gradio: Ember.inject.service(),
   classNames: 'profs-gradio'.w(),
+  filteredUpdates: function() {
+    var search = new RegExp(this.get('searchText'), 'i');
+    return Ember.ArrayProxy.createWithMixins({
+      cont: this,
+      content: Ember.computed.filter('cont.gradio.updates', function(item) {
+        if (!!(item.post.title || '').match(search)) {return true;}
+        if (!!(item.post.title || '').match(search)) {return true;}
+        if (!!(item.comment.body || '').match(search)) {return true;}
+        if (!!(item.post.author || '').match(search)) {return true;}
+        if (!!(item.comment.author || '').match(search)) {return true;}
+        return false;
+      })
+    });
+  }.property('searchText'),
+
   updates: function() {
-    return this.get('gradio.updates').slice(0, this.get('maxUpdates'));
-  }.property('gradio.updates.@each', 'maxUpdates'),
+    return this.get('filteredUpdates').slice(0, this.get('maxUpdates'));
+  }.property('filteredUpdates.@each', 'maxUpdates'),
   maxUpdates: 100,
+  searchText: '',
   actions: {
     ytEnded: function() {
       console.log('playback ended');
