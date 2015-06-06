46c3f04b59305f53a9ea07f8bedb9e8e63fd31b8
go1dfish <justageekbiz@gmail.com>
2015-06-05 19:51:14 -0700

    Hide pressiah button link when logged in

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   gradio: Ember.inject.service(),
+  snoocore: Ember.inject.service(),
 
   autoplay: Ember.computed.alias('gradio.autoplay'),
   ytid: Ember.computed.alias('gradio.lastUpdate.ytid'),
